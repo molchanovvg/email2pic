718bb327e13d3c700679697cf834aa93979d9253
molchanovvg <valeruko@gmail.com>
2016-03-20 13:39:15 +0300

    Изменена логика генерации картинки

--- generateImage.php
+++ generateImage.php
@@ -2,7 +2,11 @@
 
 function generateImg($email, $path_png){
 
-    $CAPTCHA_WIDTH = 300;
+    if (strlen($email)<8) {
+        $CAPTCHA_WIDTH=120;
+    }else{
+        $CAPTCHA_WIDTH = 15*(strlen($email)-1);
+    };
     $CAPTCHA_HEIGHT = 50;
     $PATH_FONT = 'fonts/Roboto-Regular.ttf';
 
@@ -10,11 +14,14 @@ function generateImg($email, $path_png){
     $bg_color = imagecolorallocate($img, 255, 255, 255);
     $text_color = imagecolorallocate($img, 0, 0, 0);
 
+    $bbox=imagettfbbox(18, 0, $PATH_FONT, $email );
+    $x = $bbox[0] + (imagesx($img) / 2) - ($bbox[4] / 2);
+    $y = $bbox[1] + (imagesy($img) / 2) - ($bbox[5] / 2) - 5;
+
     imagefilledrectangle($img, 0, 0, $CAPTCHA_WIDTH, $CAPTCHA_HEIGHT, $bg_color);
-    imagettftext($img, 18, 0, 5, $CAPTCHA_HEIGHT-5, $text_color, $PATH_FONT, $email);
+    imagettftext($img, 18, 0, $x, $y, $text_color, $PATH_FONT, $email);
 
     imagepng($img, $path_png);
-
     imagedestroy($img);
 }
 
